d843b4892ed2443d122c738f16e04293bc2a7a9e
Marcela Gonçalves <marcelasouzagoncalves@gmail.com>
2023-04-17 22:01:57 -0300

    Creating Empty View

--- EvercodeApp/Screens/Components/EmptyView.swift
+++ EvercodeApp/Screens/Components/EmptyView.swift
@@ -2,5 +2,37 @@ import UIKit
 
 class EmptyView: UIView {
     
-    // TODO
+    private lazy var texto: UILabel = {
+        let label = UILabel()
+        label.text = "No data found"
+        label.tintColor = .black
+        label.font = UIFont.systemFont(ofSize: 22, weight: .semibold)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    init() {
+        super.init(frame: .zero)
+        backgroundColor = .white
+        self.setupView()
+        self.setConstraints()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
+private extension EmptyView {
+    func setupView() {
+        addSubview(texto)
+    }
+    
+    func setConstraints() {
+        texto.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        texto.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+    }
+    
 }
